29aa2163370ca0785d38d92f06d6c4554ccf4f97
stf09 <bikemlit32@gmail.com>
2023-06-15 11:57:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#<include stdio.h>
+#include <stdio.h>
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by new line."
- * return 0.
+ * Return: Always 0.
  */
 int main(void)
 {

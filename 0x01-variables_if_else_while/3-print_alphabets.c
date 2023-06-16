4a67926226c88439a1691d42e2780dcbb2043aee
stf09 <bikemlit32@gmail.com>
2023-06-16 12:16:19 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * prints the alphbet in lowercase
+ * print new line
+ * then, print uppercase letters
+ * 
+ * Return:Always = 0;
+ */
+int main(void)
+{
+	char let;
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		putchar(let);
+	}
+	for  (let = 'A'; let <= 'Z'; let++)
+	{
+		putchar(let);
+	}
+
+	putchar('\n');
+	return (0);
+}

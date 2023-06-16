b00a2607c30ec645ce0aa093bd6347a9b4f88299
stf09 <bikemlit32@gmail.com>
2023-06-16 12:43:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nu;
+
+	for (nu = 0; nu < 10; nu++)
+		printf("%d", nu);
+
+	printf("\n");
+
+	return (0);
+}

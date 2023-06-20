b15a9608c8c6f88aa5b0d6872938c425e4390153
stf09 <bikemlit32@gmail.com>
2023-06-20 11:17:30 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10 - Prints 10
+ * print_alphabet_x10 - Prints 10
  * times the alplhabet
  *
  * Return : Always 0
@@ -13,11 +13,9 @@ void print_alphabet_x10(void)
 	char l;
 	int i;
 	for (i = 0; i <= 9; i++)
-	{
-		for (l = 'a'; l < 'z'; l++)
+		for (l = 'a'; l <= 'z'; l++)
 		{
 			_putchar(l);
 		}
 		_putchar('\n');
-	}
 }

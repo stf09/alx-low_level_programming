7143f7e8bfb2eb820cd0485ffe1ccaec8f3d4de3
stf09 <bikemlit32@gmail.com>
2023-06-20 10:17:37 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print _putchar
- * is written by mbah
+ *
  * Return: 0
  */
 
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowerCase
+ *
+ * Return - 0
+ */
+
+void print_alphabet(void)
+{
+
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+}

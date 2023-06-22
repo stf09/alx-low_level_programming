4469ea6e92ed0b1bf11b0ef79e6f3b994b9ebe1e
stf09 <bikemlit32@gmail.com>
2023-06-22 08:34:17 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,10 +12,10 @@ void print_integer(int m);
 void print_number(int n)
 {
 	if (n == 0)
-		_putchar('0');
+		putchar('0');
 	else if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		print_integer(n * -1);
 	}
 	else
@@ -34,6 +34,6 @@ void print_integer(int m)
 	for (; i >= 1; i /= 10)
 		if (m / i != 0)
 		{
-			_putchar((m / i) % 10 + '0');
+			putchar((m / i) % 10 + '0');
 		}
-
+}

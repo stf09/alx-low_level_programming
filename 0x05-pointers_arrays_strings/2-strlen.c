a95960720c42317d53582c8056914104283de822
stf09 <bikemlit32@gmail.com>
2023-06-25 23:24:33 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: An input string.
+ *
+ * Return: An integer
+ */
+
+int _strlen(char *s)
+{
+	int n = strlen(s);
+
+	return (n);
+
+}
+

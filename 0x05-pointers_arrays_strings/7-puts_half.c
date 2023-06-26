f651bba48d8f049d06f9410fbf0f81ea97b5c293
stf09 <bikemlit32@gmail.com>
2023-06-26 11:25:00 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,14 +20,14 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			putchar(str[i]);
+			printf(str[i]);
 		}
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			putchar(str[n + 1]);
+			printf(str[n + 1]);
 		}
 	}
-	putchar('\n');
+	printf('\n');
 }

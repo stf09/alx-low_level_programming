69fe3dbaa524513edb978a2777b3e8d5cb863e7f
stf09 <bikemlit32@gmail.com>
2023-06-26 02:17:24 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ * and in new line
+ *
+ * @s: An input string
+ * Return: reversed string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int a;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (a = i; a > 0; a--)
+	{
+		printf("%c", s[a]);
+	}
+	printf("\n");
+}
+
+

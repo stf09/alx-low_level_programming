18d58cc594f0eea76aa8f01176cb1e06ad87631d
stf09 <bikemlit32@gmail.com>
2023-06-25 23:46:35 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string with a new
+ * line
+ * @str: An input string
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
+

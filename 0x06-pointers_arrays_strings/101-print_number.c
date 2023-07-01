0bd67fac82db4794093cd97185e7d03f831afe34
stf09 <bikemlit32@gmail.com>
2023-06-30 23:07:03 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - print an integer
@@ -8,15 +9,15 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		if (n < -9)
 			print_number(n / -10);
-		_putchar('0' - n % 10);
+		putchar('0' - n % 10);
 	}
 	else
 	{
 		if (n > 9)
 			print_number(n / 10);
-		_putchar(n % 10 + '0');
+		putchar(n % 10 + '0');
 	}
 }

ef0682b6dc1ada3ae09b23f79b998e2a7787e7f0
stf09 <bikemlit32@gmail.com>
2023-07-03 00:59:39 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: string
+ * @c: the character
+ *
+ * Return: chracter or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *res;
+	
+	res = strchr(s, c);
+	return (res);
+}

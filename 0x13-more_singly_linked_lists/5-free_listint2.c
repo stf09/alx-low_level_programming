62872c7c20a62c21e8a1a2a87c391d28552486ad
stf09 <bikemlit32@gmail.com>
2023-07-31 22:44:32 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,24 +1,25 @@
 #include "lists.h"
 
 /**
- * free_listint2 -  frees a listint_t list
- * @head: is a pointer to struct
- * Return: None
- */
+* free_listint2 -  frees a listint_t list
+* @head: is a pointer to struct
+* Return: None
+*/
 
 void free_listint2(listint_t **head)
 {
-	listint_t *trsh;
-	if (head == NULL)
-	{
-		return;
-	}
+listint_t *trsh;
 
-	while (*head)
-        {
-                trsh = (*head)->next;
-                free(*head);
-		(*head) = trsh;
-        }
-	(*head) = NULL;
+if (head == NULL)
+{
+return;
+}
+
+while (*head)
+{
+trsh = (*head)->next;
+free(*head);
+(*head) = trsh;
+}
+(*head) = NULL;
 }

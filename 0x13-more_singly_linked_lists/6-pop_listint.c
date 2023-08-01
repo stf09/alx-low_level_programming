9963e8a3acacc84044b4c966ae713d902659d2cf
stf09 <bikemlit32@gmail.com>
2023-08-01 06:08:10 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,25 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * pop_listint - deletes the head node and
- * @head: pointer to the struct
- * Return: returns the head's node.
- */
-
+  * pop_listint - pops off the head of the list and returns its contents
+  * @head: head of the list
+  * Return: contents of head
+  */
 int pop_listint(listint_t **head)
 {
-	listint_t *delhead;
-	int h;
+	int i;
+	listint_t *current, *tmp;
 
 	if (head == NULL)
-	{
 		return (0);
+	tmp = current = *head;
+	if (*head)
+	{
+		i = current->n;
+		*head = current->next;
+		free(tmp);
 	}
-	h = (*head)->n;
-	delhead = (*head);
-	*head = (*head)->next;
-	free(delhead);
-	return (h);
+	else
+		i = 0;
+	return (i);
 }

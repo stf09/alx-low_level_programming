6030ff56afb6bf7c5434da157e0b4bb07918334a
stf09 <bikemlit32@gmail.com>
2023-07-31 22:36:13 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node and
+ * @head: pointer to the struct
+ * Return: returns the head's node.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *delhead;
+	int h;
+
+	if (head == NULL)
+	{
+		return 0;
+	}
+	h = (*head)->n;
+	delhead = (*head);
+	*head = (*head)->next;
+	free(delhead);
+	return (h);
+}

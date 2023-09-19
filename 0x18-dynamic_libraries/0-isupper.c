3e1f093bd38855fc25b46c0de8f6637bc2ebf13b
stf09 <bikemlit32@gmail.com>
2023-09-19 06:58:14 +0000

    2-strlen.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - prints if the characters are lowercase or not
+ * @c: the character being tested
+ * Return: 0 for lowercase, 1 for uppercase
+ */
+int _isupper(int c)
+{
+	if ('A' <= c && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
